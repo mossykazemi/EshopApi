cbb42297d520375172dc025579abac3d5cd3cc00
Mostafa Kazemi <m.kazemi.512@gmail.com>
2021-07-25 12:21:34 +0430

    I Want to Use Repository Pattern for this Test Api

---
+++ Contracts/ICustomerRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using EshopApi.Models;
+
+namespace EshopApi.Contracts
+{
+    public interface ICustomerRepository
+    {
+        IEnumerable<Customer> GetAll();
+        Task<Customer> Add(Customer customer);
+        Task<Customer> Find(int id);
+        Task<Customer> Update(Customer customer);
+        Task<Customer> Remove(int id);
+        Task<bool> IsExists(int id);
+    }
+}
