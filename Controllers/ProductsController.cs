bbde13931626ba5c94ac65bb02597d23e17a8c0b
Mostafa Kazemi <m.kazemi.512@gmail.com>
2021-07-26 09:35:04 +0430

    Added ProductController

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EshopApi.Contracts;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using EshopApi.Models;
+
+namespace EshopApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private IProductRepository _productRepository;
+
+        public ProductsController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        // GET: api/Products
+        [HttpGet]
+        public IEnumerable<Product> GetProducts()
+        {
+            return _productRepository.GetAll();
+        }
+
+        // GET: api/Products/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProduct([FromRoute] int id)
+        {
+            var product = await _productRepository.Find(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(product);
+        }
+
+        // PUT: api/Products/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutProduct(int id, Product product)
+        {
+            if (id != product.ProductId)
+            {
+                return BadRequest();
+            }
+
+            await _productRepository.Update(product);
+
+            return NoContent();
+        }
+
+        // POST: api/Products
+        [HttpPost]
+        public async Task<IActionResult> PostProduct(Product product)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            await _productRepository.Add(product);
+
+            return CreatedAtAction("GetProduct", new { id = product.ProductId }, product);
+        }
+
+        // DELETE: api/Products/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProduct(int id)
+        {
+            var product = await _productRepository.Find(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            await _productRepository.Remove(id);
+
+            return Ok(product);
+        }
+
+        private async Task<bool> ProductExists(int id)
+        {
+            return await _productRepository.IsExist(id);
+        }
+    }
+}
