0d322aa523453fe8d8ad1ca0eeb05af7d78f2a9d
Mostafa Kazemi <m.kazemi.512@gmail.com>
2021-07-26 08:52:23 +0430

    Added Two More Interfaces

---
+++ Contracts/IProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using EshopApi.Models;
+
+namespace EshopApi.Contracts
+{
+    public interface IProductRepository
+    {
+        IEnumerable<Product> GetAll();
+        Task<Product> Add(Product product);
+        Task<Product> Find(int id);
+        Task<Product> Update(Product product);
+        Task<Product> Remove(int id);
+        Task<bool> IsExist(int id);
+    }
+}
---
+++ Contracts/ISalesPersonRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using EshopApi.Models;
+
+namespace EshopApi.Contracts
+{
+    public interface ISalesPersonRepository
+    {
+        IEnumerable<SalesPerson> GetAll();
+        Task<SalesPerson> Add(SalesPerson salesPerson);
+        Task<SalesPerson> Find(int id);
+        Task<SalesPerson> Update(SalesPerson salesPerson);
+        Task<SalesPerson> Remove(int id);
+        Task<bool> IsExist(int id);
+    }
+}
