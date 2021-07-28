ace1cf6758075e73f9ee5cc9b09b78be65e3d808
Mostafa Kazemi <m.kazemi.512@gmail.com>
2021-07-28 15:44:52 +0430

    Added Ajax FormData to Send data to Api . + Recieved Data in Api Controller and saved it

--- WebApiSaveImage/Controllers/PeopleController.cs
+++ WebApiSaveImage/Controllers/PeopleController.cs
@@ -2,7 +2,9 @@
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using WebApiSaveImage.Models;
 
@@ -23,6 +25,42 @@ namespace WebApiSaveImage.Controllers
         public ActionResult<IEnumerable<Person>> Get()
         {
             return _context.Persons;
+        }
+
+        [HttpPost]
+        public IActionResult post()
+        {
+            var data = HttpContext.Request.Form;
+            var dic = Request.Form
+                .ToDictionary(x => x.Key, x => x.Value.ToString());
+            Person person = new Person();
+
+            foreach (var kvp in data.Keys)
+            {
+                PropertyInfo pi =
+                    person.GetType()
+                        .GetProperty(kvp, BindingFlags.Public | BindingFlags.Instance);
+                if (pi != null)
+                {
+                    pi.SetValue(person, dic[kvp], null);
+                }
+            }
+
+            if (data.Files.Count > 0)
+            {
+                IFormFile img = data.Files[0];
+                person.ImageName = img.FileName;
+                string savePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images", person.ImageName);
+                using (var fileStream = new FileStream(savePath, FileMode.Create))
+                {
+                    img.CopyTo(fileStream);
+                }
+            }
+
+            _context.Persons.Add(person);
+            _context.SaveChanges();
+
+            return Ok(person);
         }
     }
 }
